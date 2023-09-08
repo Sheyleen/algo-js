dcabb9728ff79d1102760fb8ce3a6b70e7887bfd
Justine Frigo <justine0912@gmail.com>
2023-09-08 11:37:33 +0200

    2.1

---
+++ Control-structures/2.1.js
@@ -0,0 +1,9 @@
+let userAge = prompt("Please enter your age:");
+
+let age = parseInt(userAge);
+
+if (age >= 18) {
+  alert("You are an adult.");
+} else {
+  alert("You are not yet an adult.");
+}
