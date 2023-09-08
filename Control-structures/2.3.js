c1489b73a66ad384fb0865d19fc8d25b6d689ad6
Justine Frigo <justine0912@gmail.com>
2023-09-08 12:04:16 +0200

    2.3

---
+++ Control-structures/2.3.js
@@ -0,0 +1,17 @@
+for(i=1; i<=100; i++){
+    if(i%2==0){
+        console.log(i);
+    }
+}
+
+i=1;
+while(i<=100){
+    if(i%2==0){
+        console.log(i + " is even.");
+        i++;
+    }
+    else{
+        console.log(i + " is odd.");
+        i++;
+    }
+}
