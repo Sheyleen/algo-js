9561da304667792d3e252c7cbeac3926a6375ad3
Justine Frigo <justine0912@gmail.com>
2023-09-11 09:18:37 +0200

    3.0

---
+++ 03-Arrays/3.0.js
@@ -0,0 +1,32 @@
+let students = [
+  "Alexandre Va",
+  "Alexandre Ve",
+  "Antoine",
+  "Bastien",
+  "Carole",
+  "Dorian",
+  "Elisabeth",
+  "Elodie",
+  "Justin",
+  "Justine F",
+  "Justine L",
+  "Kimi",
+  "Layla",
+  "Lidwine",
+  "Lucas",
+  "Marie",
+  "Mathias",
+  "Okly",
+  "Pierre",
+  "Robin",
+  "Rosalie",
+  "Stéphane",
+  "Tim",
+  "Thomas",
+  "Valentin",
+  "Virginie",
+];
+
+for (let student of students) {
+  console.log(student);
+}
