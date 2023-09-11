7e1a8910806a8ca28bc1a4b9093df90730d3a8ad
Justine Frigo <justine0912@gmail.com>
2023-09-11 09:41:23 +0200

    3.3

---
+++ 03-Arrays/3.3.js
@@ -0,0 +1,10 @@
+let games = ["WoW", "LoL", "FFXIV"];
+let gamesCopy = [];
+gamesCopy.push("WoW", "LoL", "FFXIV");
+
+console.log(games, gamesCopy);
+
+let animes = ["Naruto", "Fairy Tail", "Death Note"];
+let animesCopy = [...animes];
+
+console.log(animes, animesCopy);
